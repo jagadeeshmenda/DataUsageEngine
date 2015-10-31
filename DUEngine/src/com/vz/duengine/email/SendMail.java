82f9a72c3ff8e2dfbe708995b39681a9e09e9b04
Jagadeesh <jagadeeesh.mendas@gmail.com>
2015-10-31 11:50:06 +0530

    email changes done
    change done by Jagadeesh.

--- DUEngine/src/com/vz/duengine/email/SendMail.java
+++ DUEngine/src/com/vz/duengine/email/SendMail.java
@@ -10,36 +10,39 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+
 import com.vz.duengine.model.NotifyModel;
 
 public class SendMail {
 
-	public static void sendEmail(NotifyModel notifyModel) {
+	public void sendEmail(NotifyModel notifyModel) {
 
-		final String username = "chiddu.m@gmail.com";
-		final String password = "1234567";
+		final String username = "noreply.vzdatausage@gmail.com";
+		final String password = "Gladitors123";
 
+		
 		Properties props = new Properties();
 		props.put("mail.smtp.auth", "true");
 		props.put("mail.smtp.starttls.enable", "true");
-		props.put("mail.smtp.host", "smtp.googlemail.com");
+		props.put("mail.smtp.host", "smtp.gmail.com");
 		props.put("mail.smtp.port", "587");
 
-		Session session = Session.getInstance(props,
-		  new javax.mail.Authenticator() {
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(username, password);
-			}
-		  });
-
+		
+		Session session = Session.getInstance(props, new GMailAuthenticator(username, password));
+		
+		// set the less security level at  
+		//https://www.google.com/settings/security/lesssecureapps
+		// source - Google Docs.
 		try {
 
 			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress("chiddu.m@gmail.com"));
+			message.setFrom(new InternetAddress("noreply.vzdatausage@gmail.com"));
 			message.setRecipients(Message.RecipientType.TO,
 				InternetAddress.parse(notifyModel.getEmail()));
-			String subject  = " Data Usage alert.." ;
-			String body = "Yout total Data Usage :" + notifyModel.getUsage();
+			String subject  = " Data Usage alert..!" ;
+			String body = " Dear Customer,  \n\nYout total Data Usage :" + notifyModel.getUsage() /1024 /1024 + "MB\n\n\n Regards, \nGladitors";
 			
 			message.setSubject(subject);
 			message.setText(body);
@@ -53,4 +56,19 @@ public class SendMail {
 			System.out.println("sent email..");
 		}
 	}
+	
+	
+	public static void main(String[] args){
+		NotifyModel nfm  =  new NotifyModel () ;
+		
+		nfm.setEmail("jagadee.me@gmail.com,jasmeet.iimc@gmail.com");
+		nfm.setState("AP");
+		nfm.setUsage(2000000000);
+		SendMail sm  = new SendMail (); 
+		sm.sendEmail(nfm);
+		
+		
+	}
+	
+	
 }
